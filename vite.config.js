96f1ff05acf7b9d9be8e6bac5668afce985749d0
ConnectiVoz <connect@ivoz.ai>
2025-09-05 10:43:38 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-export default {
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "https://rivoz.in",
+        target: "https://agent.ivoz.ai",
         changeOrigin: true,
-        secure: false,
+        secure: false, // set to true if SSL cert is valid
       },
     },
   },
-};
+});
